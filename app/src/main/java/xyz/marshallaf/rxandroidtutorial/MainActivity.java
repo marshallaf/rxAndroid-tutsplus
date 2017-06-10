7b0fcdf7377888d4a0558a27472c2250099af0eb
Andrew Marshall <marshall.a.f@gmail.com>
2017-06-10 14:34:25 -0500

    Create observer for demo observable, with Observer lifecycle log messages

--- app/src/main/java/xyz/marshallaf/rxandroidtutorial/MainActivity.java
+++ app/src/main/java/xyz/marshallaf/rxandroidtutorial/MainActivity.java
@@ -2,19 +2,25 @@ package xyz.marshallaf.rxandroidtutorial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends Activity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // create an observable that will emit a sequence of integers
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
@@ -28,5 +34,31 @@ public class MainActivity extends Activity {
                 e.onComplete();
             }
         });
+
+        // create an observer that will subscribe to our observable
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.e(LOG_TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.e(LOG_TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(LOG_TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(LOG_TAG, "onComplete: Observable is finished emitting!");
+            }
+        };
+
+        // subscribe the observer to the observable
+        observable.subscribe(observer);
     }
 }
