0d798648b8cdaf4551671671f508e9a4ab3d5b62
Andrew Marshall <marshall.a.f@gmail.com>
2017-06-10 14:46:37 -0500

    Create observable on separate thread from main UI

--- app/src/main/java/xyz/marshallaf/rxandroidtutorial/MainActivity.java
+++ app/src/main/java/xyz/marshallaf/rxandroidtutorial/MainActivity.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
 
@@ -21,30 +20,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // create an observable that will emit a sequence of integers
-        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
-                // onNext emits each item in the stream
-                e.onNext(1);
-                e.onNext(2);
-                e.onNext(3);
-                e.onNext(4);
-
-                // call onComplete when the observable has emitted all the items in the sequence
-                e.onComplete();
-            }
-        });
+        Observable observable = Observable.just(1, 2, 3)
+                .subscribeOn(Schedulers.newThread());
 
         // create an observer that will subscribe to our observable
         Observer<Integer> observer = new Observer<Integer>() {
             @Override
             public void onSubscribe(@NonNull Disposable d) {
-                Log.e(LOG_TAG, "onSubscribe: ");
+                Log.e(LOG_TAG, "onSubscribe called on thread " + Thread.currentThread().getName() + ".");
             }
 
             @Override
             public void onNext(@NonNull Integer integer) {
-                Log.e(LOG_TAG, "onNext: " + integer);
+                Log.e(LOG_TAG, "onNext: " + integer + " on thread " + Thread.currentThread().getName());
             }
 
             @Override
@@ -54,7 +42,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onComplete() {
-                Log.e(LOG_TAG, "onComplete: Observable is finished emitting!");
+                Log.e(LOG_TAG, "onComplete called on thread " + Thread.currentThread().getName() + ". Observable is finished emitting!");
             }
         };
 
